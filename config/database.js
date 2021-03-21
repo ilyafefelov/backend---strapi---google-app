465b4a1dd421887ae70c2ec3b586c7d5787cc40a
Illya Fefelov <ilyafefelov@gmail.com>
2021-03-21 14:25:24 +0200

    mongo

--- config/database.js
+++ config/database.js
@@ -1,15 +1,40 @@
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: 'mongoose',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        uri: env("URI_DB"),
+        host: env('DATABASE_HOST'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
       },
       options: {
-        useNullAsDefault: true,
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        useUnifiedTopology: false,
+        slaveOk: true,
+        retryWrites: true,
+        ssl: env.bool('DATABASE_SSL', true),
       },
     },
   },
 });
+
