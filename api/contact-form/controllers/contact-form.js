ede5b88640cd1be841ac0c5452e3d8a95e0e0d9a
Illya Fefelov <ilyafefelov@gmail.com>
2022-02-16 19:02:38 +0200

    tetis-jobs

--- api/contact-form/controllers/contact-form.js
+++ api/contact-form/controllers/contact-form.js
@@ -1,8 +1,72 @@
-'use strict';
+const {
+  parseMultipartData,
+  sanitizeEntity
+} = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  async create(ctx) {
+    let entity;
+    console.log("ctx:", ctx);
+    // return await strapi.services
+    if (ctx.is('multipart')) {
+      const {
+        data,
+        files
+      } = parseMultipartData(ctx);
+      entity = await strapi.services["contact-form"].create(data, {
+        files
+      });
+    } else {
+      entity = await strapi.services["contact-form"].create(ctx.request.body);
+    }
+    entity = await sanitizeEntity(entity, {
+      model: strapi.models["contact-form"]
+    });
 
-module.exports = {};
+    await strapi.plugins["email"].services.email
+      .send({
+        to: "fefelov@full-iron.online",
+        from: "fefelov@full-iron.online",
+        subject: "Contact form posted",
+        // TODO: repond to
+        text: `
+        The comment #${entity.id} was posted to contact form.
+
+        Content:
+        ${JSON.stringify(entity, null, "\t")}
+      `,
+        html: `
+          <div style="display: table; flex-direction: column;">
+          <h2>Contact form #${entity.id} was posted from tetis.com</h2>
+
+          <h5>From:</h5>
+          <p>from (name): ${entity.name}</p>
+          <p>phone: ${entity.phone}</p>
+          <p>email: ${entity.email}</p>
+
+          <h5>Message:</h5>
+          <p style="font-size: 16px;
+                background: #f1eee9;
+                font-weight: 400;
+                font-style: italic;
+                padding: 0.8em;">
+            ${entity.message}
+          </p>
+
+          <span style="left: 33%; position: relative;">-----------------------------------</span>
+          <h6>Full data:</h6>
+          <pre style="white-space: pre-line;
+            tab-size: 2;
+            background: #f8fcff;
+            padding: 0.5em;
+            margin-top: -1em;">
+            ${JSON.stringify(entity, null, "\t")}
+        </pre>
+        </div>
+      `,
+      })
+      .then(console.log("email sent. data"));
+
+    return entity
+  },
+};
--- api/index-page/models/index-page.js
+++ api/index-page/models/index-page.js
@@ -5,28 +5,29 @@ const axios = require('axios');
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
-
-module.exports = {lifecycles: {
-  afterCreate: async entry => {
-    axios
-      .post(strapi.config.get('server.staticWebsiteBuildURL'), {})
-      .catch(() => {
-        // Ignore
-      });
-  },
-  afterUpdate: async entry => {
-    // console.log("entry update",strapi.config.currentEnvironment, strapi.config.get('server.staticWebsiteBuildURL'))
-    axios
-      .post(strapi.config.get('server.staticWebsiteBuildURL'), {})
-      .catch(() => {
-        // Ignore
-      });
-  },
-  afterDestroy: async entry => {
-    axios
-      .post(strapi.config.get('server.staticWebsiteBuildURL'), {})
-      .catch(() => {
-        // Ignore
-      });
-  },
-}};
+module.exports = {
+  lifecycles: {
+    afterCreate: async entry => {
+      axios
+        .post(strapi.config.get('server.staticWebsiteBuildURL'), {})
+        .catch(() => {
+          // Ignore
+        });
+    },
+    afterUpdate: async entry => {
+      console.log("entry update",strapi.config.currentEnvironment, strapi.config.get('server.staticWebsiteBuildURL'))
+      axios
+        .post(strapi.config.get('server.staticWebsiteBuildURL'), {})
+        .catch(() => {
+          // Ignore
+        });
+    },
+    afterDestroy: async entry => {
+      axios
+        .post(strapi.config.get('server.staticWebsiteBuildURL'), {})
+        .catch(() => {
+          // Ignore
+        });
+    },
+  }
+};
